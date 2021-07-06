c6bbbf913aee4e44e708d7f285dc2933f0c2d9c0
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-07-06 16:00:03 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+var n1 = prompt("Ingrese un primer numero: ")
+var n2 = prompt("Ingrese un segundo numero: ")
+
+document.write("La suma es : ");
+document.write(parseint(n1) + parseInt(n2));
+document.write(" ");
+document.write("La resta es : ");
+document.write(parseint(n1) - parseInt(n2));
+document.write(" ");
+document.write("La multiplicacion es : ");
+document.write(parseint(n1) * parseInt(n2));
+document.write(" ");
+document.write("La division es : ");
+document.write(parseint(n1) / parseInt(n2));
+document.write(" ");
+
+//////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+
