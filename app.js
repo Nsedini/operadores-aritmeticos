6d232c2ca4014a70006399ab9f66754ed4e106ce
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-07-08 12:18:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ let n1 = prompt("Ingrese un primer numero: ")
 let n2 = prompt("Ingrese un segundo numero: ")
 
 document.write("La suma es : ");
-document.write(parseint(n1) + parseInt(n2));
+document.write(parseInt(n1) + parseInt(n2))</br>;
 document.write(" ");
 document.write("La resta es : ");
-document.write(parseint(n1) - parseInt(n2));
+document.write(parseInt(n1) - parseInt(n2))</br>;
 document.write(" ");
 document.write("La multiplicacion es : ");
-document.write(parseint(n1) * parseInt(n2));
+document.write(parseInt(n1) * parseInt(n2)); </br>
 document.write(" ");
 document.write("La division es : ");
-document.write(parseint(n1) / parseInt(n2));
+document.write(parseInt(n1) / parseInt(n2));
 document.write(" ");
 
 //////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
 let temperature = prompt("Ingrese la temperatura °C: ")
+document.write("La temperatura en Kelvin es")
+document.write(parseInt(temperature) + 273.15);
 
 
 
